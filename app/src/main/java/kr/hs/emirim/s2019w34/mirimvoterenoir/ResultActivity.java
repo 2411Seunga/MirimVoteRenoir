f49c3eceb6a67523a1b16343d0afbbef150fa4ff
LeeSeungA <seunga4170@gmail.com>
2020-11-24 20:38:01 +0900

    Modify & Update ResultActivity

--- app/src/main/java/kr/hs/emirim/s2019w34/mirimvoterenoir/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w34/mirimvoterenoir/ResultActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -24,6 +25,22 @@ public class ResultActivity extends AppCompatActivity {
 
         int[] textIds = {R.id.text1, R.id.text2, R.id.text3, R.id.text4, R.id.text5, R.id.text6, R.id.text7, R.id.text8, R.id.text9};
         int[] ratingIds = {R.id.rating_bar1, R.id.rating_bar2, R.id.rating_bar3, R.id.rating_bar4, R.id.rating_bar5, R.id.rating_bar6, R.id.rating_bar7, R.id.rating_bar8, R.id.rating_bar9};
+        int[] imgFileNames = {R.drawable.img01, R.drawable.img02, R.drawable.img03, R.drawable.img04, R.drawable.img05, R.drawable.img06, R.drawable.img07, R.drawable.img08, R.drawable.img09};
+
+        int max = 0;
+        int maxIndex = 0;
+
+        for (int i = 0; i < imgNames.length; i++) {
+            if(voteCount[i] > max) {
+                max = voteCount[i];
+                maxIndex = i;
+            }
+        }
+
+        TextView textMaxTitle = findViewById(R.id.text_max_title);
+        ImageView imgvMax = findViewById(R.id.imgv_max);
+        textMaxTitle.setText(imgNames[maxIndex]);
+        imgvMax.setImageResource(imgFileNames[maxIndex]);
 
         for (int i = 0; i < imgNames.length; i++) {
             textVs[i] = findViewById(textIds[i]);
